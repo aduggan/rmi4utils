633f5a365e59f3c5ba0b2e71af99634d0aa409cf
Andrew de los Reyes <adlr@google.com>
2015-09-04 14:13:03 -0700

    RMIDevice: Make dtor vitual
    To avoid compilation warning

--- rmidevice/rmidevice.h
+++ rmidevice/rmidevice.h
@@ -32,6 +32,7 @@ class RMIDevice
 public:
 	RMIDevice() : m_functionList(), m_sensorID(0), m_bCancel(false), m_bytesPerReadRequest(0), m_page(-1)
 	{}
+	virtual ~RMIDevice() {}
 	virtual int Open(const char * filename) = 0;
 	virtual int Read(unsigned short addr, unsigned char *data,
 				unsigned short len) = 0;
