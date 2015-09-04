1c7a596e98a7767f5406b8aa05f5aecf0eda5733
Andrew de los Reyes <adlr@google.com>
2015-09-04 14:11:16 -0700

    Android.mk fixes for include paths

--- f54test/Android.mk
+++ f54test/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := f54test
-LOCAL_C_INCLUDES := rmidevice
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../rmidevice
 LOCAL_SRC_FILES := main.cpp f54test.cpp testutil.cpp display.cpp
 LOCAL_CPPFLAGS := -Wall
 LOCAL_STATIC_LIBRARIES := rmidevice
--- rmi4update/Android.mk
+++ rmi4update/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := rmi4update
-LOCAL_C_INCLUDES := rmidevice
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../rmidevice
 LOCAL_SRC_FILES := main.cpp rmi4update.cpp updateutil.cpp firmware_image.cpp
 LOCAL_CPPFLAGS := -Wall
 LOCAL_STATIC_LIBRARIES := rmidevice
